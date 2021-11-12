5c135d2b7dd499092ad9f58c8a3d00f896080bc9
steveforrest <steveforrest_1@hotmail.com>
2021-11-12 17:35:27 +0000

    made changes2

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sf-blogged.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['sf-blogged.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
